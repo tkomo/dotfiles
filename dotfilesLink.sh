c23a7c6ab1fe5cc624da35346bb76ae3ae14580a
root <root@ms751.maximo.com>
2014-08-13 14:17:57 +0900

    initial commit

---
+++ dotfilesLink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#ln -sf ~/dotfiles/.vimrc ~/.vimrc
+#ln -sF ~/dotfiles/colors ~/.vim/colors
+#ln -sF ~/dotfiles/ftdetect ~/.vim/ftdetect
+#ln -sF ~/dotfiles/indent ~/.vim/indent
+#ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+#ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
+#ln -sf ~/dotfiles/Brewfile ~/Brewfile
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
