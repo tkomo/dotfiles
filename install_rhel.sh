21fe542f59b1e57cf08f9bb45a50258e1cc46ce7
root <root@mycentos.local>
2014-11-17 23:35:35 +0900

    Handle with homesick

--- install_rhel.sh
+++ install_rhel.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
+#!/bin/zsh
 
 # yum
-yum -y install zsh
-
-# get dotfiles
-git submodule init
-git submodule update
+yum -y install gcc openssl-devel
 
 # chsh
 chsh -s /bin/zsh
 
 # rbenv, ruby, rails
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-exec $SHELL -l
+source ~/.zshrc
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 rbenv install -v 2.1.2
