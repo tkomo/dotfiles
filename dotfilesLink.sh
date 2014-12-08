41f66359bb6eaf55e205233ecd2e84acd64f3a9a
root <root@mycentos.local>
2014-12-08 14:14:00 +0900

    rename to setup.sh

--- dotfilesLink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/home/.vimrc ~/.vimrc
-#ln -sF ~/dotfiles/colors ~/.vim/colors
-#ln -sF ~/dotfiles/ftdetect ~/.vim/ftdetect
-#ln -sF ~/dotfiles/indent ~/.vim/indent
-#ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-#ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-#ln -sf ~/dotfiles/Brewfile ~/Brewfile
-ln -sf ~/dotfiles/home/.zshrc ~/.zshrc
