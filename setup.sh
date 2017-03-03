76c31acc8c7145d9a8dde490dc98221e53fb3b8b
tkomo <tkomo@example.com>
2017-03-03 17:28:50 +0900

    brush up

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-sudo chsh -s /bin/zsh `whoami`
+chsh -s /bin/zsh `whoami`
 
 git submodule init
 git submodule update
 
-ln -sf ~/dotfiles/home/.vimrc ~/.vimrc
-#ln -sF ~/dotfiles/colors ~/.vim/colors
-#ln -sF ~/dotfiles/ftdetect ~/.vim/ftdetect
-#ln -sF ~/dotfiles/indent ~/.vim/indent
-#ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-#ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-#ln -sf ~/dotfiles/Brewfile ~/Brewfile
-ln -sf ~/dotfiles/home/.zshrc ~/.zshrc
+cd home
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    ln -sf ~/dotfiles/home/${f} ~/${f} && echo "${f}"
+done
